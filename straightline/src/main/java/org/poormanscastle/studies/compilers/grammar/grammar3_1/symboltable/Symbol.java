1a6f17cf11e584115a7fa63b76f4a9ffdfc91cbe
Georg Federmann <georg.federmann@gmail.com>
2016-02-10 11:03:06 +0100

    started implementation of the symbol table for semantic analysis of abstract syntax trees.

---
+++ straightline/src/main/java/org/poormanscastle/studies/compilers/grammar/grammar3_1/symboltable/Symbol.java
@@ -0,0 +1,17 @@
+package org.poormanscastle.studies.compilers.grammar.grammar3_1.symboltable;
+
+/**
+ * a symbol is an entity in the grammar 3.1 like a variable name.
+ * <p/>
+ * when new concepts get implemented, like methods and custom types, their declarations two get stored
+ * on the symbol tables.
+ * Created by 02eex612 on 29.01.2016.
+ */
+public class Symbol {
+
+    /**
+     * the name of this symbol;
+     */
+    private String name;
+
+}

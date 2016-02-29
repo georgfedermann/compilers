6ff6303a207f18fd20dfdf99d3faca3d30af6337
Georg Federmann <georg.federmann@gmail.com>
2016-02-29 14:51:29 +0100

    implementing symbol table

---
+++ straightline/src/main/java/org/poormanscastle/studies/compilers/utils/grammartools/ast/symboltable/Environment.java
@@ -0,0 +1,48 @@
+package org.poormanscastle.studies.compilers.utils.grammartools.ast.symboltable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.poormanscastle.studies.compilers.utils.grammartools.ast.Binding;
+import org.poormanscastle.studies.compilers.utils.grammartools.ast.Symbol;
+
+/**
+ * an environment is a collections of symbols that have been declared and are known
+ * in the current scope. Environments can be nested und thus symbols can be
+ * inherited to nested environments. Environments can also be referenced as is the
+ * case with e.g. static class members: T.b the field b in class T. So T is a kind
+ * of environment here.
+ * <p/>
+ * new environments are created when new scopes are added, like blocks of a conditional
+ * statement. When a scope goes out of scope (end of a function, end of a block) the
+ * whole environment gets removed.
+ * <p/>
+ * Created by 02eex612 on 29.02.2016.
+ */
+public class Environment {
+
+    private Map<Symbol, Binding> bindings = new HashMap<>();
+
+    public Binding addSymbol(String name, String type) {
+        Symbol symbol = Symbol.getSymbol(name);
+        if (bindings.containsKey(symbol)) {
+            throw new RuntimeException(StringUtils.join("Symbol already in symbol table:", symbol));
+        }
+        Binding binding = new Binding(type);
+        bindings.put(symbol, binding);
+        return binding;
+    }
+
+    public boolean contains(Symbol symbol) {
+        return bindings.containsKey(symbol);
+    }
+
+    public Binding getSymbol(Symbol symbol) {
+        return bindings.get(symbol);
+    }
+
+    public int getSize() {
+        return bindings.size();
+    }
+}
--- straightline/src/main/java/org/poormanscastle/studies/compilers/utils/grammartools/ast/symboltable/SymbolTable.java
+++ straightline/src/main/java/org/poormanscastle/studies/compilers/utils/grammartools/ast/symboltable/SymbolTable.java
@@ -1,11 +1,12 @@
 package org.poormanscastle.studies.compilers.utils.grammartools.ast.symboltable;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.poormanscastle.studies.compilers.utils.grammartools.ast.Binding;
 import org.poormanscastle.studies.compilers.utils.grammartools.ast.Symbol;
+
+import static com.google.common.base.Preconditions.checkState;
 
 /**
  * 1st version of a symbol table for the still straightline language v0.1.
@@ -13,12 +14,20 @@ import org.poormanscastle.studies.compilers.utils.grammartools.ast.Symbol;
  * even a simple sequence of declaration statements will create multiple environments.
  * Along with a sequence of declaration statements, different identifiers and their bindings will
  * become available.
- *
+ * <p/>
  * Created by 02eex612 on 19.02.2016.
  */
 public class SymbolTable {
 
-    private Map<Symbol, Binding> bindings = new HashMap<>();
+    private List<Environment> environments = new LinkedList<>();
+
+    /**
+     * initializies the SymbolTable and adds the sigma zero environment.
+     */
+    public SymbolTable() {
+        checkState(environments.isEmpty());
+        newScope();
+    }
 
     /**
      * this simple single scope SymbolTable implementation will not accept mutliple symbols with the same name.
@@ -26,19 +35,32 @@ public class SymbolTable {
      * @param name
      * @param type
      */
-    public void addSymbol(String name, String type) {
-        Symbol symbol = Symbol.getSymbol(name);
-        if (bindings.containsKey(symbol)) {
-            throw new RuntimeException(StringUtils.join("Symbol already in symbol table:", symbol));
-        }
-        bindings.put(symbol, new Binding(type));
+    public Binding addSymbol(String name, String type) {
+        checkState(!environments.isEmpty());
+        return environments.get(0).addSymbol(name, type);
     }
 
-    public Binding getBinding(Symbol symbol){
+    public void newScope() {
+        environments.add(0, new Environment());
+    }
+
+    public void endScope() {
+        checkState(!environments.isEmpty());
+        environments.remove(0);
+    }
+
+    public Binding getBinding(Symbol symbol) {
+        for(Environment environment : environments){
+            if(environment.contains(symbol)){
+                return environment.getSymbol();
+            }
+        }
+
+
         return bindings.get(symbol);
     }
 
-    public int getSize(){
+    public int getSize() {
         return bindings.size();
     }
 
